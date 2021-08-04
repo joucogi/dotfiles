4d2fb80fa0927821d2a481fd1637cdb2d7825068
Joel Coll Costa <joel.coll@gmail.com>
2021-08-04 18:36:58 +0200

    Dump packages

--- shell/exports.sh
+++ shell/exports.sh
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 export CODELY_THEME_MINIMAL=false
 export CODELY_THEME_MODE="dark"
-export CODELY_THEME_PROMPT_IN_NEW_LINE=false
+export CODELY_THEME_PROMPT_IN_NEW_LINE=true
 export CODELY_THEME_PWD_MODE="short" # full, short, home_relative
 
 # ------------------------------------------------------------------------------
