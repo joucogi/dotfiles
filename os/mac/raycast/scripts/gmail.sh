1e6dd3edb277277bc949bdffa919dc0fd55a232e
Joel Coll <joel.coll@gmail.com>
2021-09-15 08:34:16 +0200

    Raycast scripts and settings

---
+++ os/mac/raycast/scripts/gmail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Gmail
+# @raycast.packaName Utils
+# @raycast.mode silent
+# @raycast.icon 📨
+# @raycast.description Open the Gmail website
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "https://mail.google.com/mail/u/0/#inbox"
---
+++ os/mac/raycast/scripts/twitter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Twitter
+# @raycast.packaName Utils
+# @raycast.mode silent
+# @raycast.icon 🐦
+# @raycast.description Open the Twitter website
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "https://twitter.com/home" --profile-directory=Default
